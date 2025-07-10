68d150b6b3e1357a2544b6c9ee8ef0bbfe0992e5
Donato Laynes <donato.laynes@outlook.es>
2025-07-10 12:35:34 -0500

    Arreglar navegacion

---
+++ src/main/java/com/grupo2/clinicasalud/controller/ControllerUtils.java
@@ -0,0 +1,45 @@
+package com.grupo2.clinicasalud.controller;
+
+import com.grupo2.clinicasalud.model.Medico;
+import com.grupo2.clinicasalud.model.Paciente;
+import com.grupo2.clinicasalud.model.Usuario;
+import com.grupo2.clinicasalud.service.auth.DetalleUsuarioService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class ControllerUtils {
+
+    @Autowired
+    DetalleUsuarioService detalleUsuarioService;
+
+    @ModelAttribute("requestURI")
+    String getRequestURI(HttpServletRequest request) {
+        return request.getRequestURI();
+    }
+
+    @ModelAttribute("nombreUsuario")
+    String getNombreUsuario( @AuthenticationPrincipal Usuario user){
+        if(user.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("Admin"))){
+            return "Admin";
+        }
+        if(user.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("Doctor"))){
+            Medico medico = detalleUsuarioService.getMedicoActual();
+            if(medico == null){
+                return "Nombre Doctor";
+            }
+            return medico.getApellido() + ", " + medico.getNombre();
+        }
+        if(user.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("Cliente"))){
+            Paciente paciente = detalleUsuarioService.getPacienteActual();
+            if(paciente == null){
+                return "Nombre Paciente";
+            }
+            return paciente.getApellido() + ", " + paciente.getNombre();
+        }
+        return "Desconocido";
+    }
+}
